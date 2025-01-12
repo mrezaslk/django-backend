3513b914b974ba8750da4ba5f55f94c6eba16a20
mohammadreza salek <rezasalek75@gmail.com>
2025-01-12 07:53:36 +0330

    [mod]create new movies project

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-expsv20223s_^$v&18d2lij-q(3jfjpkv+*41k=um7nhrurj7!'
+SECRET_KEY = 'django-insecure-ruz3gr=d+7it3ta5(3hx#08eiqy-nntwv@w6u#3xjgv=rxab$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app'
+    # local app
+    'movies',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from my_app.views import index, about, hello, add
+from movies.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('about/', about),
-    path('hello/<str:first_name>', hello),
-    path('add/<int:num1>/<int:num2>', add),
 ]
