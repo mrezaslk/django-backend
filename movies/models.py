8b6ccd9e5c9ad03cb979288b767d855c21c41d63
mohammadreza salek <rezasalek75@gmail.com>
2025-01-14 19:17:54 +0330

    add database model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# mode; => python class
+# model rep a table in the db
+# attr are the fields in the table
+
+
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+
+# command for migration
+# make migration =>> python manage.py makemigrations
+# migration
--- movies/views.py
+++ movies/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 
-def index(request):
-    context = {
-        'movies':['gladiator','anything','test']
-    }
-    return render(request, 'movies/index.html', context)
+# def index(request):
+#     context = {
+#         'movies':['gladiator','anything','test']
+#     }
+#     return render(request, 'movies/index.html', context)
 
 # app/templates/app/index.html
 
 
-def about(request):
-    return render(request, 'movies/about.html')
+# def about(request):
+#     return render(request, 'movies/about.html')
+
+
+def index(request):
+    return HttpResponse('<h1>hello</hello>')
